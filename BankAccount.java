7b59c5a0207f52dd95d7adac811899ac6a652fb0
Monerah alshathri <monerah3232@gmail.com>
2026-07-02 11:28:04 +0300

    added deposit and withdraw methods and hashCode-equals

--- BankAccount.java
+++ BankAccount.java
@@ -1,22 +1,48 @@
 public class BankAccount {
-    double Balance;
+    private int accountNumber;
+    private double balance;
 
-    public double UpdateBalance(char oprator , double amount) {
-        if (oprator == '+')
-            Balance = Balance + amount;
-        else if (oprator =='-')
-            Balance= Balance- amount;
-        else
-            System.out.println("please enter right oprator");
+    public BankAccount(int accountNumber, double balance){
+        this.accountNumber=accountNumber;
+        this.balance=balance;
+    }
 
-        return Balance;
-            }
+    public double deposit(double amount) {
+        return balance = balance + amount;
+    }
+    public double withdraw(double amount) {
+        return balance = balance - amount;
+    }
 
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj)
+            return true;
+
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        BankAccount other = (BankAccount) obj;
+
+        return accountNumber == other.accountNumber;
+    }
+
+    @Override
+    public int hashCode(){
+        return Integer.hashCode(accountNumber);
+    }
+    public int getAccountNumber(){
+        return accountNumber;
+    }
+    public void setAccountNumber(int accountNumber){
+        this.accountNumber=accountNumber;
+    }
     public double getBalance() {
-        return Balance;
+        return balance;
     }
 
     public void setBalance(double balance) {
-        Balance = balance;
+
+        this.balance = balance;
     }
 }
--- Main.java
+++ Main.java
@@ -5,20 +5,35 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        BankAccount account = new BankAccount();
-
         System.out.println("enter your balance:");
-        double UserBalance = scanner.nextDouble();
-        account.setBalance(UserBalance);
+        double userBalance = scanner.nextDouble();
+        //account.setBalance(userBalance);
+
+        BankAccount account = new BankAccount(0,userBalance);
 
         System.out.println("enter + to deposit  OR - to withdraw");
-        char oprator = scanner.next().charAt(0);
+        char operator = scanner.next().charAt(0);
 
         System.out.println("enter the amount:");
         double amount = scanner.nextDouble();
 
-        double result = account.UpdateBalance(oprator, amount);
+        double result ;
+
+        if (operator == '+')
+            result = account.deposit(amount);
+        else if (operator =='-')
+            result = account.withdraw(amount);
+        else
+            throw new IllegalArgumentException("Invalid operator");
+
         System.out.println("your Balance is :" + result);
+
+        BankAccount a1 = new BankAccount(1,300);
+        BankAccount a2 = new BankAccount(1,500);
+
+        System.out.println("----equals method Override-----");
+        System.out.println(a1 == a2);
+        System.out.println(a1.equals(a2));
     }
 }
 
